beeb9539de5c0589d37d7a60440a457df293b135
Dan D'Avella <drdavella@gmail.com>
2017-09-20 20:20:32 -0400

    Remove doctest plus plugin from astropy core

--- setup.py
+++ setup.py
@@ -61,7 +61,6 @@ entry_points['console_scripts'] = [
 entry_points['pytest11'] = [
     # Don't collide with the 'doctestplus' plugin name that will be registered
     # by this plugin's pytest_configure hook
-    '_doctestplus = astropy.tests.plugins.doctestplus',
     'repeat = astropy.tests.plugins.repeat',
     'config = astropy.tests.plugins.config',
     'display = astropy.tests.plugins.display',
