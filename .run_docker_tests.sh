c253ff217e251fc1e27c9f6a86b262da5922e522
Dan D'Avella <drdavella@gmail.com>
2017-09-25 13:52:03 -0400

    Update circleci script to install pytest plugins from github

--- .run_docker_tests.sh
+++ .run_docker_tests.sh
@@ -20,6 +20,11 @@ python3 -c 'import sys; print(sys.maxsize)'
 # we install an older version here
 easy_install-3.5 pytest pytest-xdist
 
+# Temporary update to support testing against new standalone pytest plugins
+pip install git+https://github.com/astropy/pytest-doctestplus.git
+pip install git+https://github.com/astropy/pytest-remotedata.git
+pip install git+https://github.com/astropy/pytest-openfiles.git
+
 # Install git
 apt-get install -y git
 
